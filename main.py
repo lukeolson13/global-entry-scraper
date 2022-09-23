9f5bb3f00cd752e647f71d11b5b283e9abc248ac
Luke Olson <luke@overlayanalytics.com>
2022-09-22 18:48:47 -0600

    Fix a bumch of awful typing and clean things up a bit; jitter the delay; if no appointments are available, try again; send an email when something is available; handle bad requests

--- main.py
+++ main.py
@@ -1,75 +1,86 @@
 import argparse
-from config import WEBHOOK_URL
 from datetime import datetime
-from discord_webhook import DiscordWebhook
+import logging
+import random
+from typing import List, Dict
 import requests
 import time
 
+import yagmail as yagmail
+
+from secrets import APP_PASS, SEND_TO, SEND_FROM
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
 
 REQUEST_DELAY = 2
 TIMESLOT_URL = "https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit={limit}&locationId={location_id}&minimum=1"
 MAPPING_URL = "https://ttp.cbp.dhs.gov/schedulerapi/locations/?temporary=false&inviteOnly=false&operational=true&serviceName=Global%20Entry"
 
 
-def import_mapping_from_url() -> dict:
+def delay() -> float:
+    return REQUEST_DELAY + random.randint(-10, 10) / (10 * REQUEST_DELAY / 2)  # jitter
+
+
+def make_request(url: str):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise ValueError(f'Non-200 code returned ({resp.status_code}): {resp.json()}')
+
+    return resp.json()
+
+
+def import_mapping_from_url() -> Dict[int, str]:
     """Get mapping of location ids to location names from the TTP website."""
-    r = requests.get(MAPPING_URL)
+    resp_dict = make_request(MAPPING_URL)
     return {
         location["id"]: "{} ({}, {})".format(
             location["name"], location["city"], location["state"]
         )
-        for location in r.json()
+        for location in resp_dict
     }
 
 
-def parse_timeslot_datetime(timeslot: dict) -> dict:
+def parse_timeslot_datetime(timeslot: dict) -> datetime:
     """Parse the timestamp of a single timeslot."""
     return datetime.strptime(timeslot["startTimestamp"], "%Y-%m-%dT%H:%M")
 
 
-def get_timeslots_for_location_id(location_id: int, limit: int) -> set:
+def get_timeslots_for_location_id(location_id: int, limit: int) -> List[datetime]:
     """Get list of objects representing open slots for a certain location."""
-    r = requests.get(TIMESLOT_URL.format(location_id=location_id, limit=limit))
-    timeslots = [parse_timeslot_datetime(timeslot) for timeslot in r.json()]
+    resp_dict = make_request(TIMESLOT_URL.format(location_id=location_id, limit=limit))
+
+    timeslots = [parse_timeslot_datetime(timeslot) for timeslot in resp_dict]
     return sorted(list(set(timeslots)))
 
 
-def get_timeslots_for_location_ids(
-    location_ids: list, before: str = None, limit: int = 10
-) -> list:
+def get_timeslots_for_location_ids(location_ids: List[int], before: str = None, limit: int = 10,
+                                   ) -> Dict[int, List[datetime]]:
     """Get a mapping of location ids to open timeslots. Takes in an optional YYYY-MM-DD
     parameter to filter the results."""
     all_timeslots = {}
-    for index, location_id in enumerate(location_ids):
+    for location_id in location_ids:
         timeslots = [
             timeslot
             for timeslot in get_timeslots_for_location_id(location_id, limit)
-            if before is None or datetime.strptime(before, "%Y-%m-%d") > timeslot
+            if before is None or timeslot < datetime.strptime(before, "%Y-%m-%d")
         ]
         all_timeslots[location_id] = timeslots
-        if index < len(location_ids) - 1:  # delay between requests.
-            time.sleep(REQUEST_DELAY)
+        time.sleep(delay())
+
     return all_timeslots
 
 
-def generate_notification_texts(
-    location_mapping: dict, all_timeslots: dict, silent: bool
-) -> None:
+def generate_notification_texts(location_mapping: Dict[int, str], all_timeslots: Dict[int, List[datetime]],
+                                silent: bool) -> List[str]:
     """Generate the text for the notification."""
     notification_texts = ["✈️ Global Entry Timeslot Bot ✈️"]
     for location_id, timeslots in all_timeslots.items():
         if len(timeslots) > 0:
-            location_texts = []
-            location_texts.append(
-                "{} ({})".format(location_mapping[location_id], location_id)
-            )
+            location_texts = [f"{location_mapping[location_id]} ({location_id})"]
             for timeslot in timeslots:
                 location_texts.append(
-                    "    {}".format(
-                        timeslot.strftime("%B %d, %Y (%a) @ %I:%M %p").replace(
-                            " 0", " "
-                        )
-                    )
+                    f'    {timeslot.strftime("%B %d, %Y (%a) @ %I:%M %p").replace(" 0", " ")}'
                 )
             notification_texts.append("\n".join(location_texts))
     if len(notification_texts) == 1:
@@ -80,21 +91,22 @@ def generate_notification_texts(
     return notification_texts
 
 
-def send_to_discord(notification_texts: str) -> None:
+def send(notification_texts: List[str]):
     """Send a notification to Discord."""
-    for index, message in enumerate(notification_texts):
-        webhook = DiscordWebhook(url=WEBHOOK_URL, content=message)
-        webhook.execute()
-        if index < len(notification_texts) - 1:  # delay between messages.
-            time.sleep(REQUEST_DELAY)
+    for message in notification_texts:
+        print(message)
+
+    yag = yagmail.SMTP(user=SEND_FROM, password=APP_PASS)
+    yag.send(to=SEND_TO, subject='Global Entry - book Quick!', contents=notification_texts)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--location-ids",
-        nargs="+",
+        "--location-ids", '-l',
+        action='append',
         type=int,
+        default=[],
         help="List of location ids to check",
         required=True,
     )
@@ -106,6 +118,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--limit",
         type=int,
+        default=10,
         help="Limit of appointments to fetch, per location",
     )
     parser.add_argument(
@@ -115,15 +128,17 @@ if __name__ == "__main__":
         help="Suppress notifications if there are no open timeslots",
     )
 
-    args = vars(parser.parse_args())
-    kwargs = {
-        k: args[k] for k in ["location_ids", "before", "limit"] if args[k] is not None
-    }  # strip empty params for parameters when getting timeslots.
+    args = parser.parse_args()
 
-    location_mapping = import_mapping_from_url()
-    all_timeslots = get_timeslots_for_location_ids(**kwargs)
-    notification_text = generate_notification_texts(
-        location_mapping, all_timeslots, args["silent"]
-    )
+    loc_mapping = import_mapping_from_url()
 
-    send_to_discord(notification_text)
+    keep_running = True
+    while keep_running:
+        ats = get_timeslots_for_location_ids(args.location_ids, args.before, args.limit)
+        if any(val for val in ats.values()):
+            keep_running = False
+        else:
+            time.sleep(delay())
+
+    notification_text = generate_notification_texts(loc_mapping, ats, args.silent)
+    send(notification_text)
